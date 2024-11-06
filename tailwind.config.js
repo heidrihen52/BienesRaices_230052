7d96e1f768b1ab1fc495d8b1b54d99bd27a690cb
Adrian Perez Jimenez <230052@utxicotepec.edu.mx>
2024-11-06 16:01:49 -0600

    Practica 12
    Agregacion de los colores personalizados al proyecto en el archivo tilwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ['./views/**/*.pug'],
   theme: {
     extend: {},
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'black': '#000000',
+      'white': '#ffffff',
+      'green': '#2F855A',
+      'orange': '#FF6F00',
+      'beige': '#D4B88C',
+      'gray': '#606060',
+      'bubblegum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
   },
   plugins: [],
 }
